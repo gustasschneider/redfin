6de87accfb4808652dd0196f2c9381342a681195
my-name <gustavo.farias@genesisgroup.com.br>
2022-08-08 10:17:44 -0300

    add arquivos do bootstrap v.5.2

--- views/login.php
+++ views/login.php
@@ -1,22 +1,32 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Login</title>
-        <link href="<?php echo BASE_URL; ?>/assets/css/login.css" rel="stylesheet" />
+        <link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/js/bootstrap.min.js" rel="stylesheet"/>
+        <link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
     </head>
-    <body>
-        <div class="loginarea">
+    <body class="text-center">
+        <main class="form-signin w-100 m-auto">
         	<form method="POST">
-        		<input type="email" name="email" placeholder="Digite seu e-mail" />
 
-        		<input type="password" name="password" placeholder="Digite sua senha" />
+                <div class="form-floating">
+                    <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Digite seu e-mail</label>
+                </div>
 
-        		<input type="submit" value="Entrar" /><br/>
+                <div class="form-floating">
+                    <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Digite sua senha</label>
+                </div>
+                <br>
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
+                <br/>
 
         		<?php if(isset($error) && !empty($error)): ?>
         		<div class="warning"><?php echo $error; ?></div>
         		<?php endif; ?>
         	</form>
-        </div>
+        </main>
     </body>
 </html>
