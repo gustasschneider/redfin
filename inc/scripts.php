2fcb447567179416abce9f5109dcc1f4140bdf3f
my-name <gustavo.farias@genesisgroup.com.br>
2022-08-08 13:44:48 -0300

    add arquivos do bootstrap icons

---
+++ inc/scripts.php
@@ -0,0 +1,3 @@
+<link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/js/bootstrap.min.js" rel="stylesheet"/>
+<link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
+<link href="<?= BASE_URL;?>/node_modules/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"/>
--- views/login.php
+++ views/login.php
@@ -1,16 +1,17 @@
+<?php
+require_once ('inc/scripts.php');
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Login</title>
         <link href="<?= BASE_URL;?>/assets/css/login.css" rel="stylesheet"/>
-        <link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/js/bootstrap.min.js" rel="stylesheet"/>
-        <link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
     </head>
     <body class="bodyBckgColor">
         <main class="w-100 m-auto form-entrar">
         	<form method="POST">
-                <h1 class="h3 mb-3 fw-normal text-center">Entrar</h1>
+                <h1 class="h3 mb-3 fw-normal text-center">Entrar <i class="bi bi-bookmark-star text-danger"></i></h1>
                 <div class="form-floating">
                     <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                     <label for="floatingInput">Digite seu e-mail</label>
@@ -22,7 +23,7 @@
                 </div>
                 <br>
                 <button class="w-100 btn btn-lg btn-danger" type="submit">Entrar</button>
-                <br/>
+                <p class="mt-5 mb-3 text-muted text-center">© Redfin - 2022</p>
 
         		<?php if(isset($error) && !empty($error)): ?>
         		<div class="warning"><?php echo $error; ?></div>
