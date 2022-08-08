32ba8a565709c4ba984c590c6185b78622fb2800
my-name <gustavo.farias@genesisgroup.com.br>
2022-08-08 10:17:26 -0300

    add arquivos do bootstrap v.5.2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'd711a68a93cf43a7693a0c9b56ad6993625087c9',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'd711a68a93cf43a7693a0c9b56ad6993625087c9',
             'dev_requirement' => false,
         ),
         'container-interop/container-interop' => array(
@@ -72,6 +72,21 @@
             'aliases' => array(),
             'reference' => '30605874d99af0cde6c41fd39e18546330c38100',
             'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => '5.2.0',
+            'version' => '5.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'reference' => 'edf9c40956d19e6ab3f9151bfe0dfac6be06fa21',
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '5.2.0',
+            ),
         ),
         'zendframework/zend-loader' => array(
             'pretty_version' => '2.5.1',
