587170d69ad1019d3920263274b273f0f9be7846
my-name <gustavo.farias@genesisgroup.com.br>
2022-08-10 11:14:56 -0300

    ajustes no layout do arquivo template pt1

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,6 +1,12 @@
 <link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/js/bootstrap.min.js" rel="stylesheet"/>
 <link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
 <link href="<?= BASE_URL;?>/node_modules/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"/>
+<link href="<?= BASE_URL;?>/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" rel="stylesheet"/>
+
+
+
+
+
 <script type="text/javascript" src="assets/js/jquery-3.6.0.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
--- views/template.php
+++ views/template.php
@@ -1,3 +1,6 @@
+<?php
+require_once ('inc/scripts.php');
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -13,22 +16,22 @@
     			<?php echo $viewData['company_name']; ?>
     		</div>
             <div class="menuarea">
-                <ul>
-                    <li><a href="<?php echo BASE_URL; ?>">Home</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>/permissions">Permissões</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>/users">Usuários</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>/clients">Clientes</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>/inventory">Estoque</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>/sales">Vendas</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>/purchases">Compras</a></li>
-                    <li><a href="<?php echo BASE_URL; ?>/report">Relatórios</a></li>
+                <ul class="nav flex-column">
+                    <li><a href="<?php echo BASE_URL; ?>"><i class="bi bi-house-door-fill"></i> Home</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>/permissions"><i class="bi bi-key-fill"></i> Permissões</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>/users"><i class="bi bi-person-circle"></i> Usuários</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>/clients"><i class="bi bi-bank2"></i> Clientes</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>/inventory"><i class="bi bi-basket-fill"></i> Estoque</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>/sales"><i class="bi bi-currency-exchange"></i> Vendas</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>/purchases"><i class="bi bi-cart-fill"></i> Compras</a></li>
+                    <li><a href="<?php echo BASE_URL; ?>/report"><i class="bi bi-journal-text"></i> Relatórios</a></li>
                 </ul>
             </div>
     	</div>
     	<div class="container">
     		<div class="top">
     			<div class="top_right"><a href="<?php echo BASE_URL.'/login/logout'; ?>">Sair</a></div>
-    			<div class="top_right"><?php echo $viewData['user_email']; ?></div>    			
+    			<div class="top_right"><?php echo $viewData['user_email']; ?></div>
     		</div>
             <div class="area">
                 <?php $this->loadViewInTemplate($viewName, $viewData); ?>
